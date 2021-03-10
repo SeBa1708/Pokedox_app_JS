20e6732afde2e37ba76a97fbb3f390c90346e6fb
SeBa1708 <77842208+SeBa1708@users.noreply.github.com>
2021-03-10 15:41:50 +0100

    Added a for loop with the conditional to highlight a Pokemon if height > 1,5 m

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 "use strict";
 
 let pokemonList = [
-  {
-  name: 'balbasure',
-  type:['grass','poisson'],
-  height:'0.7m'
-  },
-
-  {
-  name: 'pikachu',
-  type:'electric',
-  height:'0.4m'
-  },
-
-  {
-  name: 'Jigglypuff',
-  type:['fairy','normal'],
-  height:'0.7m'
-  },
+  {name: 'balbasure', type:['grass','poisson'], height: 0.7},
+  {name: 'pikachu', type:'electric', height: 0.4},
+  {name: 'Jigglypuff', type:['fairy','normal'],height: 2}
 ];
 
-console.log(pokemonList);
+
+//Part 1 Running a for loop to write all information about the pokemons is the DOM!
+
+for (let i = 0; i < pokemonList.length; i++){
+document.write(`Name: ${pokemonList[i].name} Height: ${pokemonList[i].height} m <br/> <br/>` )};
+
+// Part 3  Running a for loop to write all information about the pokemons is the DOM!
+//Adding a conditional (height 1.5) to check if the pokemon is above a certain value.
+
+for (let i = 0; i < pokemonList.length; i++)
+{
+if (pokemonList[i].height > 1.5) {
+  document.write(`Name: ${pokemonList[i].name} - Height: ${pokemonList[i].height} m ---> That is huge <br/> <br/>` );
+  } else {
+      document.write (`Name: ${pokemonList[i].name} - Height: ${pokemonList[i].height} m <br/> <br/>`)
+  }
+};
