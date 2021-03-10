4fd69af56f7105ec9d5fae76a12599a67501a3eb
SeBa1708 <77842208+SeBa1708@users.noreply.github.com>
2021-03-10 10:21:20 +0100

    Created Pokemon List with 3 Objekts and 3 keys

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let pokemonList = [
+  {
+  name: 'balbasure',
+  type:['grass','poisson'],
+  height:'0.7m'
+  },
+
+  {
+  name: 'pikachu',
+  type:'electric',
+  height:'0.4m'
+  },
+
+  {
+  name: 'Jigglypuff',
+  type:['fairy','normal'],
+  height:'0.7m'
+  },
+];
+
+console.log(pokemonList);
