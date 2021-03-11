454c821295716ec8cb454471ab9f94e2d1824bdf
SeBa1708 <77842208+SeBa1708@users.noreply.github.com>
2021-03-11 19:28:28 +0100

    Using for each loop instead of for loop

--- script.js
+++ script.js
@@ -6,20 +6,10 @@ let pokemonList = [
   {name: 'Jigglypuff', type:['fairy','normal'],height: 2}
 ];
 
-
-//Part 1 Running a for loop to write all information about the pokemons is the DOM!
-
-for (let i = 0; i < pokemonList.length; i++){
-document.write(`Name: ${pokemonList[i].name} Height: ${pokemonList[i].height} m <br/> <br/>` )};
-
-// Part 3  Running a for loop to write all information about the pokemons is the DOM!
-//Adding a conditional (height 1.5) to check if the pokemon is above a certain value.
-
-for (let i = 0; i < pokemonList.length; i++)
-{
-if (pokemonList[i].height > 1.5) {
-  document.write(`Name: ${pokemonList[i].name} - Height: ${pokemonList[i].height} m ---> That is huge <br/> <br/>` );
-  } else {
-      document.write (`Name: ${pokemonList[i].name} - Height: ${pokemonList[i].height} m <br/> <br/>`)
-  }
-};
+// for each loop instead of for loop 
+pokemonList.forEach(function(pokemon) {if (pokemon.height > 1) {
+    document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is huge' + '<br> </br>');
+} else {
+    document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is normal' +'<br> </br>');
+}
+});
