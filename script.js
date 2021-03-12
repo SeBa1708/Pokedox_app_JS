0ed9dc2dd83a38d24866cb8eb28d100af539ba81
SeBa1708 <77842208+SeBa1708@users.noreply.github.com>
2021-03-12 14:50:14 +0100

    Refactoring: Using IIFE structure and using for each loop
    To retrieve data: using getAll key
    To add Pokemon: Using add and push keys.

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-"use strict";
 
-let pokemonList = [
-  {name: 'balbasure', type:['grass','poisson'], height: 0.7},
-  {name: 'pikachu', type:'electric', height: 0.4},
-  {name: 'Jigglypuff', type:['fairy','normal'],height: 2}
+// IIFE Structure
+var pokemonRepository = (function () {
+  var pokemonList = [
+    { name: 'balbasure', type: ['grass', 'poisson'], height: 0.7 },
+    { name: 'pikachu', type: 'electric', height: 0.4 },
+    { name: 'Jigglypuff', type: ['fairy', 'normal'], height: 2 },
 ];
 
-// for each loop instead of for loop 
-pokemonList.forEach(function(pokemon) {if (pokemon.height > 1) {
-    document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is huge' + '<br> </br>');
-} else {
-    document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is normal' +'<br> </br>');
-}
+// Function add pokemon
+function add(pokemon){
+    pokemonList.push(pokemon);
+  }
+
+// Function to retrieve data of the pokemonList
+  function getAll() {
+    return pokemonList;
+  }
+
+// Using the same name for keys and values
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+// Call the function
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+/* Original for each loop
+pokemonList.forEach(function (pokemon) {if (pokemon.height > 1) {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is huge '
+      + '<br> </br>'); } else {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is normal '
+      + '<br> </br>');}
+*/
+
+// Using the foreach Method to iterate over the pokemonList to decide which pokemon is big oder normal.
+pokemonRepository.getAll//using the getAll function to retrieve data
+().forEach(function (pokemon) {if (pokemon.height > 1) {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is huge '
+      + '<br> </br>'); } else {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is normal '
+      + '<br> </br>');}
 });
+
+
+
+/* Normal for each method out of the IIFE scope! --> Task 1
+pokemonList.forEach(function (pokemon) {if (pokemon.height > 1) {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is huge '
+      + '<br> </br>'); } else {
+      document.write('Name:' + pokemon.name + '- Height' + pokemon.height + 'm ---> That is normal '
+      + '<br> </br>');}
+*/
